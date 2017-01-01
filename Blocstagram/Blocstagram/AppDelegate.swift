f3b62496d7e50045df52dfed407f70ccfabb6dd8
Dulio Denis <dulio.denis@yahoo.com>
2017-01-01 18:39:44 -0500

    Customized the tab bar tint color and added Nav Controllers

--- Blocstagram/Blocstagram/AppDelegate.swift
+++ Blocstagram/Blocstagram/AppDelegate.swift
@@ -13,5 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // change the tint color on the Tab Bar to Wet Asphalt
+        UITabBar.appearance().tintColor = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1)
+        
+        return true
+    }
+    
 }
 
