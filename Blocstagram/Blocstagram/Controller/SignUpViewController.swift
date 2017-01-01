2ed3433843b94af1d7e2578d165118fe7a506278
Dulio Denis <dulio.denis@yahoo.com>
2017-01-01 15:40:08 -0500

    Added the SignUpVC UI in Storyboard

---
+++ Blocstagram/Blocstagram/Controller/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Blocstagram
+//
+//  Created by ddenis on 1/1/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func dismissOnTap(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
