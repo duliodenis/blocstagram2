dc6a46547901bb1d29942892af6c8de412d00e5e
Dulio Denis <dulio.denis@yahoo.com>
2017-01-01 13:15:44 -0500

    Added the SignInVC UI in a new Storyboard

---
+++ Blocstagram/Blocstagram/Controller/SignInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.swift
+//  Blocstagram
+//
+//  Created by ddenis on 1/1/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+}
