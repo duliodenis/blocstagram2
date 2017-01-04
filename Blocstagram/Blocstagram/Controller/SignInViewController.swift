b7f285b3b4f3e1bbf9846509981271d64d686996
Dulio Denis <dulio.denis@yahoo.com>
2017-01-03 21:48:08 -0500

    Programmatically customized the SignInVC UI

--- Blocstagram/Blocstagram/Controller/SignInViewController.swift
+++ Blocstagram/Blocstagram/Controller/SignInViewController.swift
@@ -10,4 +10,42 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let clouds = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1)
+        
+        emailTextField.backgroundColor = .clear
+        emailTextField.tintColor = .white
+        emailTextField.textColor = .white
+        emailTextField.borderStyle = .none
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: clouds])
+        
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        emailTextField.clipsToBounds = true
+        
+        passwordTextField.backgroundColor = .clear
+        passwordTextField.tintColor = .white
+        passwordTextField.textColor = .white
+        passwordTextField.borderStyle = .none
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: clouds])
+        
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerPassword)
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        passwordTextField.clipsToBounds = true
+        
+        signInButton.layer.cornerRadius = 5
+    }
+    
 }
