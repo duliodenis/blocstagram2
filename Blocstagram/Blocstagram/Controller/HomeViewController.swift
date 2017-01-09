06e5f5d20c4801d4b9eecef952844a5de592f225
Dulio Denis <dulio.denis@yahoo.com>
2017-01-09 13:06:21 -0500

    Add a LogOut UIBarButtonItem to the HomeVC for sign out

--- Blocstagram/Blocstagram/Controller/HomeViewController.swift
+++ Blocstagram/Blocstagram/Controller/HomeViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+    }
+    
+    
+    // MARK: - Log Out User Method
+    
+    @IBAction func logout(_ sender: Any) {
+        // Log out user from Firebase
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch let signOutError {
+            print("Sign Out Error: \(signOutError.localizedDescription)")
+            return
+        }
+        
+        // Present the Sign In VC
+        let storyboard = UIStoryboard(name: "Start", bundle: nil)
+        let signInVC = storyboard.instantiateViewController(withIdentifier: "SignInViewController")
+        present(signInVC, animated: true)
     }
     
 }
