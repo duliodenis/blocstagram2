341c33cb3618ae16d02ea914294e783976a5dcfd
Dulio Denis <dulio.denis@yahoo.com>
2017-01-31 21:40:15 -0500

    Connect the IB collection view and header to the ProfileVC

---
+++ Blocstagram/Blocstagram/Controller/ProfileHeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderCollectionReusableView.swift
+//  Blocstagram
+//
+//  Created by Dulio Denis on 1/31/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCollectionReusableView: UICollectionReusableView {
+        
+}
--- Blocstagram/Blocstagram/Controller/ProfileViewController.swift
+++ Blocstagram/Blocstagram/Controller/ProfileViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        collectionView.dataSource = self
+    }
+    
+}
+
+
+extension ProfileViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerViewCell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "ProfileHeaderCollectionReusableView", for: indexPath) as! ProfileHeaderCollectionReusableView
+        headerViewCell.backgroundColor = UIColor.red
+        return headerViewCell
     }
     
 }
